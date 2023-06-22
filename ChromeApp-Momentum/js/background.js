2cbbefd36d96a12e92195106bb995ce340e5d4cb
Kim-Mingyeong <kalsrud0412@gmail.com>
2023-06-22 17:00:07 +0900

    rename folder

--- ChromeApp-Momentum/js/background.js
+++ ChromeApp-Momentum/js/background.js
@@ -1,5 +1,3 @@
-//새로고침 시 이미지 자동 변환
-
 const images = [
   "Midnight in Paris-1.png",
   "Midnight in Paris-2.png",
@@ -8,6 +6,7 @@ const images = [
   "Midnight in Paris-5.png",
 ];
 
+//새로고침 시 이미지 자동 변환
 const randomImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img"); //img element 생성
 bgImage.src = `img/${randomImage}`; //img 경로 설정
