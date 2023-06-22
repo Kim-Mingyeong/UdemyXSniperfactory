266bd30b7f14ba1d1b56c2d14a9eb26d5893eebb
Kim-Mingyeong <kalsrud0412@gmail.com>
2023-06-23 00:08:48 +0900

    8회차 과제

--- ChromeApp-Momentum/js/todo.js
+++ ChromeApp-Momentum/js/todo.js
@@ -7,24 +7,28 @@ let toDos = []; //create array
 
 function saveToDos() {
   //ToDos array의 내용을 local storage에 저장
-  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); //just string ["a", "b", "c"]
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); //object
 }
 
 function deleteToDo(event) {
   //event.target : HTML element, 모든 HTML element에는 하나 이상의 property가 있음
-  const li = event.target.parentElement;
+  const li = event.target.parentElement; //li.id : string
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id)); //click한 li.id와 다른 것만 남겨두기
+  saveToDos();
 }
 
 function paintToDo(newToDo) {
   const li = document.createElement("li");
-  const span = document.createElement("span");
-  span.innerText = newToDo;
+  li.id = newToDo.id;
   const button = document.createElement("button");
-  button.innerHTML = "x";
+  button.innerHTML = "❌";
+  const span = document.createElement("span");
+  span.innerText = newToDo.text;
+
   button.addEventListener("click", deleteToDo);
-  li.appendChild(span);
   li.appendChild(button);
+  li.appendChild(span);
 
   toDoList.appendChild(li);
 }
@@ -34,8 +38,12 @@ function handleToDoSubmit(event) {
   const newTodo = toDoInput.value; //변수에 input의 현재 value를 복사
   toDoInput.value = ""; //input value 비우기
   //console.log(newTodo, toDoInput.value);
-  toDos.push(newTodo); //array에 value저장
-  paintToDo(newTodo);
+  const newToDoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newToDoObj); // value저장
+  paintToDo(newToDoObj);
   saveToDos();
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
@@ -45,5 +53,5 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos); //parsedToDos : array ["a", "b", "c"]
   toDos = parsedToDos;
-  parsedToDos.forEach(paintToDo);
+  parsedToDos.forEach(paintToDo); //{text: "a", id:1687439794043}
 }
---
+++ ChromeApp-Momentum/js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "2a7b4267cdd06e30fb5fd2e394ca6ac7";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.weather[0].main} ${data.main.temp}`;
+      city.innerText = data.name;
+      console.log(data.weather[0]);
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
